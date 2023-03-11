89d2989e7d34a3afd480643f89e327e470bad22b
Paul Cumming <user@users-MBP.broadband>
2023-03-11 14:51:37 +0000

    Started extensions, max_occupancy att added and function/test

--- codeclan_caraoke/src/room.py
+++ codeclan_caraoke/src/room.py
@@ -6,12 +6,18 @@ class Room:
         self.name = name
         self.songs_playlist = []
         self.guest_list = []
+        self.max_occupancy = 4
     
     def check_in(self, guest):
-        self.guest_list.append(guest)
-        print()
-        print()
-        print(f"{guest.name} checked into {self.name}")
+        if len(self.guest_list) < self.max_occupancy:
+            self.guest_list.append(guest)
+            print()
+            print()
+            print(f"{guest.name} checked into {self.name}")
+        else: 
+            print()
+            print()
+            print(f"Sorry {guest.name}, {self.name} is full. Please book a new room")
     
     def check_out(self, guest):
         self.guest_list.remove(guest)
--- codeclan_caraoke/tests/test_room.py
+++ codeclan_caraoke/tests/test_room.py
@@ -9,11 +9,25 @@ class TestRoom(unittest.TestCase):
     def setUp(self):
         self.caraoke_house = CaraokeHouse("The Warbling Wren")
         self.guest_1 = Guest("Maria")
+        self.guest_2 = Guest("Tom")
+        self.guest_3 = Guest("Hakim")
+        self.guest_4 = Guest("David")
+        self.guest_5 = Guest("Elton")
         self.room_1 = Room("Room 1")
 
     def test_check_in(self):
         self.room_1.check_in(self.guest_1)
         self.assertEqual(1, len(self.room_1.guest_list))
+
+    def test_check_in_if_room_full(self):
+        self.room_1.guest_list = [
+            self.guest_1,
+            self.guest_2,
+            self.guest_3,
+            self.guest_4,
+        ]
+        self.room_1.check_in(self.guest_5)
+        self.assertEqual(4, len(self.room_1.guest_list))
 
     def test_check_out(self):
         self.room_1.guest_list = [self.guest_1]
