0c636cb2f9c97e406665e28580fb5f7718882263
Paul Cumming <user@users-MBP.broadband>
2023-03-13 21:25:25 +0000

    reorganised folders

--- codeclan_caraoke/tests/test_caraoke_house.py
+++ codeclan_caraoke/tests/test_caraoke_house.py
@@ -8,7 +8,6 @@ class TestCaraokeHouse(unittest.TestCase):
         self.caraoke_house = CaraokeHouse("The Warbling Wren")
         self.guest_5 = Guest("Jim")
 
-
     def test_open_room(self):
         self.caraoke_house.open_room("Room 1")
         self.assertEqual(1, len(self.caraoke_house.room_list))
