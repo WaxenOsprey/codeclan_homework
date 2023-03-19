54b1a827e699bf14f3d1449755e727e053a5c6bb
Paul Cumming <user@users-MBP.broadband>
2023-03-19 18:02:54 +0000

    small change

--- w3_flask_library/models/books.py
+++ w3_flask_library/models/books.py
@@ -1,10 +1,10 @@
 from models.book import Book
 
-book_1 = Book("Harry Potter and the Philosopher's Stone", "J. K. Rowling", "fantasy", False)
-book_2 = Book("A History of the Scottish People", "T. C. Smout", "history", True)
-book_3 = Book("The Last Kingdom", "Bernard Cornwall", "historical fiction", True)
-book_4 = Book("In Cold Blood", "Truman Capote", "new journalism", False)
-book_5 = Book("Things Fall Apart", "Chinua Achebe", "tragedy", False)
+book_1 = Book("Harry Potter and the Philosopher's Stone", "J. K. Rowling", "Fantasy", False)
+book_2 = Book("A History of the Scottish People", "T. C. Smout", "History", True)
+book_3 = Book("The Last Kingdom", "Bernard Cornwall", "Historical Fiction", True)
+book_4 = Book("In Cold Blood", "Truman Capote", "New Journalism", False)
+book_5 = Book("Things Fall Apart", "Chinua Achebe", "Tragedy", False)
 
 book_list = [book_1, book_2, book_3, book_4, book_5]
 
