9df55c14fc61fdcac8ade5fad5a25961ae4345a6
Paul Cumming <user@users-MBP.broadband>
2023-04-05 20:27:36 +0100

    Add todays h/w

--- p1_solar_system/models/moon.py
+++
@@ -1,8 +0,0 @@
-class Moon:
-
-    def __init__(self, name, planet, orbital_period, mean_radius, id = None):
-        self.name = name
-        self.planet = planet
-        self.orbital_period = orbital_period
-        self.mean_radius = mean_radius
-        self.id = id
---
+++ w6_hof_traveller/hw_traveller_start/models/traveller.js
@@ -0,0 +1,43 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map((journey) => {
+    return journey.startLocation
+  })
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => {
+    return journey.endLocation
+  })
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter((journey) => {
+    return journey.transport === transport;
+  })
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter((journey) => {
+    return journey.distance >= minDistance;
+  })
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((total, journey) => {
+    return total += journey.distance;
+  }, 0)
+};
+
+// nope
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  return this.journeys.values((journey) => {
+    return journey.transport;
+  })
+};
+
+
+module.exports = Traveller;
