4875f95a39a0c83d2820838352b86ab4f8eb264e
Paul_C <nopasaran@hotmail.co.uk>
2023-05-21 18:27:30 +0100

    hw

--- w12_Java_Air/src/main/java/RandomNumberGenerator.java
+++ w12_Java_Air/src/main/java/RandomNumberGenerator.java
@@ -13,9 +13,6 @@ public class RandomNumberGenerator {
     private void generateRandomNumber() {
         Random random = new Random();
         randomNumber = random.nextInt(max - min) + min;
-        if (randomNumber == 0) {
-            randomNumber = max; // Set the random number to the maximum value
-        }
     }
 
     public int getRandomNumber() {
