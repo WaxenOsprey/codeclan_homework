6dbec1e194f6c3ff4fa2f95a410a872d3c1533d6
Paul_C <nopasaran@hotmail.co.uk>
2023-05-31 16:56:15 +0100

    fixed issue with pom by adding version to spring boot maven plug in

---
+++ w14_lab_and_homework_start/WhiskyTracker/src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.codeclan.example.WhiskyTracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test") //Indicates it's a test profile so will not run DataLoader
+@SpringBootTest
+public class WhiskyTrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
